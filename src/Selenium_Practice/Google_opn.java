52cfc90d8b7d02a04dd581685a4d9454ebaede9c
Ksanjiwani <sanjiwanikandekar6776@gmail.com>
2026-03-14 21:03:31 +0530

    googlr_opeartion

---
+++ src/Selenium_Practice/Google_opn.java
@@ -0,0 +1,17 @@
+package Selenium_Practice;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Google_opn {
+	
+	public static void main(String args[])
+	{
+		WebDriver driver =  new ChromeDriver();
+		
+		driver.manage().window().maximize();
+		driver.get("https://www.google.com");
+		
+	}
+
+}
